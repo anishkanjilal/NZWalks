e5b5dfb60694649214e31acab285b4ef134b329b
anishkanjilal <kanjilalanish1996@gmail.com>
2022-09-20 00:53:13 +0530

    Changes for Data Migration and creating table in DB

---
+++ NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContextz:DbContext
+    {
+        public NZWalksDbContextz(DbContextOptions<NZWalksDbContextz> options):base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walks> walks { get; set; }
+        public DbSet<WalkDifficulty> walkDifficulty { get; set; }
+    }
+}
--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//adding dbcontext into the service collection
+builder.Services.AddDbContext<NZWalksDbContextz>(optionsAction =>
+{
+    optionsAction.UseSqlServer(builder.Configuration.GetConnectionString("NZwalks"));
+});
 
 var app = builder.Build();
 
